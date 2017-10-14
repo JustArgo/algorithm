d48cf5062c8caf49cd9fd836c26b2d5a7ffa1606
JustArgo <565148812@qq.com>
2017-10-15 00:41:18 +0800

    新增logistic回归

---
+++ logistic回归/logistic.py
@@ -0,0 +1,114 @@
+'''
+	本算法用于解决二值分类问题
+	1 给定 n个特征向量 和 其对应的 分类 labels  假设特征为2个
+	2 找出回归系数 x0 x1 x2 由于此处特征为2个 所以找3个回归系数 x0类似于 直线的截点
+	3 由于是二值问题 所以 用 sigmoid 阶跃函数 来修正 回归系数
+'''
+from numpy import *
+logSigmoid = False
+#加载数据
+def loadDataSet():
+	dataMat = []
+	labelMat = []
+	fr = open('testSet.txt')
+	for line in fr.readlines():
+		lineArr = line.strip().split()
+		dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+		labelMat.append([int(lineArr[2])])
+	return dataMat,labelMat
+
+#sigmoid 阶跃函数	
+def sigmoid(inX):
+	if logSigmoid == True:
+		print('log sigmoid start:')
+		print('in is %s and out is %s' % (inX,1.0/(1+exp(-inX))))
+		print('log sigmoid end:')
+	return 1.0/(1+exp(-inX))
+
+#梯度上升算法
+def gradAscent(dataMatIn, classLabels):
+	dataMatrix = mat(dataMatIn)
+	labelMat = mat(classLabels)
+	m,n = shape(dataMatrix)
+	alpha = 0.001
+	maxCycles = 500
+	weights = ones((n,1)) * 0.1
+	for k in range(maxCycles):
+		h = sigmoid(dataMatrix * weights)
+		#print(labelMat)
+		#print(h)
+		error = (labelMat - h)
+		#print(h)
+		#print(error)
+		#print(dataMatrix.transpose())
+		#print(alpha * dataMatrix.transpose() * error)
+		weights = weights + alpha * dataMatrix.transpose() * error
+	return weights
+	
+#dataArr,labelMat = loadDataSet()
+#weights = gradAscent(dataArr,labelMat)
+#print(weights)
+
+#画出数据集 和 logistic 回归的最佳拟合直线的函数
+def plotBestFit(wei):
+	import matplotlib.pyplot as plt
+	weights = wei
+	dataMat,labelMat = loadDataSet()
+	dataArr = array(dataMat)
+	n = shape(dataArr)[0]
+	xcord1 = []
+	ycord1 = []
+	xcord2 = []
+	ycord2 = []
+	for i in range(n):
+		if int(labelMat[i][0]) == 1:
+			xcord1.append(dataArr[i,1])
+			ycord1.append(dataArr[i,2])
+		else:
+			xcord2.append(dataArr[i,1])
+			ycord2.append(dataArr[i,2])
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+	ax.scatter(xcord2,ycord2,s=30,c='green')
+	x = arange(-8.0,8.0,0.1)
+	print(weights)
+	y = (-weights[0]-weights[1]*x)/weights[2]
+	ax.plot(x,y)
+	plt.xlabel('X1')
+	plt.ylabel('X2')
+	plt.show()
+	
+#随机梯度上升算法
+def stocGradAscent0(dataMatrix, classLabels):
+	m,n = shape(dataMatrix)
+	alpha = 0.5
+	weights = ones(n)
+	#weights = array([1.0,-1.0,1.0])
+	for i in range(m):
+		h = sigmoid(sum(dataMatrix[i]*weights))
+		error = classLabels[i] - h
+		weights = weights + alpha * error * dataMatrix[i] 
+	return weights
+	
+#改进的随机梯度上升算法
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+	m,n = shape(dataMatrix)
+	weights = ones(n)
+	for j in range(numIter):
+		dataIndex = range(m)
+		for i in range(m):
+			alpha = 4/(1.0+j+i)+0.01
+			randIndex = int(random.uniform(0,len(dataIndex)))
+			h = sigmoid(sum(dataMatrix[randIndex]*weights))
+			error = classLabels[randIndex] - h
+			weights = weights + alpha * error * dataMatrix[randIndex]
+			del(dataIndex[randIndex])
+	return weights
+		
+
+dataArr,labelMat = loadDataSet()
+weights = stocGradAscent0(dataArr,labelMat)
+print(weights)
+plotBestFit(weights)
+
--- 朴素贝叶斯/bias.py
+++ 朴素贝叶斯/bias.py
@@ -9,10 +9,16 @@
 import os
 import codecs
 import random
+import re
+from numpy import *
+import operator
 
 #变量 categories = []
 
 categories = []
+docPath = "C:\\Users\\Administrator\\Desktop\\python\\20_newsgroups\\";
+algorithmPath = "C:\\Users\\Administrator\\Desktop\\python\\algorithm\\"
+trainDocCount = 10
 # 遍历指定目录，显示目录下的所有文件名
 total_words = set('')
 def eachFile(filepath):
@@ -24,7 +30,7 @@ def eachFile(filepath):
         child = os.path.join('%s\%s' % (filepath, allDir))
         files = os.listdir(child)
         #print(files[:80])
-        for i in range(0,1):
+        for i in range(trainDocCount):
             filename = os.path.join('%s\%s' % (child, files[i]))
             fopen = codecs.open(filename)
             data = fopen.read()
@@ -35,19 +41,148 @@ def eachFile(filepath):
         #print(child)
 
 
+#停用词过滤器 字典专用 
+def stopWordsFilter4Dict(wordDataSet,stopWord):
+	tmpDict = wordDataSet.copy()
+	for key in tmpDict.keys():
+		if key.lower() in stopWord:
+			wordDataSet.pop(key)
+
+#停用词过滤器 list set专用
+def stopWordFilter4ListAndSet(wordDataSet,stopWord):
+	tmpSet = wordDataSet.copy()
+	for word in tmpSet:
+		if word.lower() in stopWord:
+			wordDataSet.remove(word)
+
+#删除非单词字符
+def noneVocaFilter(dataSet):
+	tmpSet = dataSet.copy()
+	if type(dataSet).__name__ == 'list':
+		for i in range(len(tmpSet)):
+			if re.match(r'^[a-zA-Z]*$',tmpSet[i]) == None:
+				dataSet.remove(tmpSet[i])
+				
+	elif type(dataSet).__name__ == 'set':
+		for word in tmpSet:
+			if re.match(r'^[a-zA-Z]*$',word) == None:
+				dataSet.remove(word)
+		
+	else:
+		return
 # 读取文件内容并打印
 def readFile(filename):
     fopen = open(filename, 'r') # r 代表read
     for eachLine in fopen:
         print("读取到得内容如下：",eachLine)
     fopen.close()
+
+# 将所有字符转换成小写
+def toLower(dataSet):
+	if type(dataSet).__name__ == 'list':
+		tmpSet = []
+		for i in range(len(dataSet)):
+			tmpSet[i] = dataSet[i].lower()
+		
+	elif type(dataSet).__name__ == 'set':
+		tmpSet = set('')
+		for word in dataSet:
+			tmpSet.add(word.lower())
+		
+	else:
+		return
+	return tmpSet
+#bytes 转  str
+def bytes2str(words):
+	for word in words:
+		if type(word).__name__ == 'bytes':
+			words.remove(word)
+			words.add(word.decode('iso-8859-1'))
+			
+#准备 停用词 用set效率更高
+def prepareStopWords():
+	stopWordList = set();
+	stopWordFilePath = algorithmPath+"stop_words.txt"
+	fopen = open(stopWordFilePath)
+	for word in fopen.readlines():
+		stopWordList.add(word.replace('\n',''))
+	return stopWordList
+		
+#将文档转换成向量 特定方法 每个分类 10个文档 则 200个向量  200行 1831列  再返回一个labels 列表 代表这200个向量对应的分类   向量为2值型  有则表示为1 无则表示为0
+def transferDoc2Vect(all_words):
 	
+	total_list = [] #二维列表 200行  1831列
+	labels = []
+	word_count_dict = {}
+	#初始化字典
+	for word in all_words:
+		word_count_dict[word] = 0
+	#1 先计算每个单词的数量 
+	for category in categories:
+		categoryPath = docPath + category
+		files = os.listdir(categoryPath)
+		for i in range(trainDocCount):
+			wordSet = set(open(categoryPath + "\\" + files[i],encoding='iso-8859-1').read().split())
+			bytes2str(wordSet)
+			noneVocaFilter(wordSet)
+			for word in wordSet:
+				if word in all_words:
+					word_count_dict[word] += 1
+	sortedWordCount = sorted(word_count_dict.items(),key=operator.itemgetter(1),reverse=True)
+	all_words = set('')
+	for i in range(10):#排序在前的10个单词
+		all_words.add(sortedWordCount[i][0])
+	total_words_list = list(all_words)
+	
+	for category in categories:
+		categoryPath = docPath + category
+		files = os.listdir(categoryPath)
+		for i in range(trainDocCount):
+			vect = zeros(len(all_words))#初始化一个向量
+			wordSet = set(open(categoryPath + "\\" + files[i],encoding='iso-8859-1').read().split())
+			bytes2str(wordSet)
+			noneVocaFilter(wordSet)
+			for word in wordSet:
+				if word in all_words:
+					vect[total_words_list.index(word)] = 1
+					break
+			total_list.append(vect)
+			labels.append(category)
+	
+	return total_list,labels
+	
+#根据返回的向量 和 标签 计算 每个分类的向量 
+def calcTotalVect(vect,labels):
+	total_vect = []
+	for category in categories:
+		category_vect = zeros(len(vect[0]))
+		for i in range(len(vect)):
+			if labels[i] == category:  #当前循环的向量 分类 为 category
+				category_vect += vect[i]
+		total_vect.append(category_vect)
+	return total_vect
+	
+#根据分类向量 计算概率
+def calcTotalProb(vect):
+	total_prob = []
+	for i in range(len(vect)):
+		total_prob.append(vect[i]/sum(vect[i]))
+	return total_prob
 eachFile("C:\\Users\\Administrator\\Desktop\\python\\20_newsgroups")
-#print(categories)
-#print(total_words)
-total_words = (b'the',b'many',b'to',b'and',b'news',b'have',b'for',b'not',b'as',b'of')
-#print('------------')
-#print(total_words)
+bytes2str(total_words) #将 bytes 转换成 str
+noneVocaFilter(total_words)
+total_words = toLower(total_words)
+stopWords = prepareStopWords()
+stopWordFilter4ListAndSet(total_words,stopWords)
+vect,labels = transferDoc2Vect(total_words)
+total_vect = calcTotalVect(vect,labels)
+total_prob = calcTotalProb(total_vect)
+print(total_vect)
+print(total_prob)
+exit()
+
+				
+
 def countWord(filepath):
 	files =  os.listdir(filepath)
 	word_dict = {}
@@ -62,7 +197,7 @@ def countWord(filepath):
 #
 all_word_dict = {}
 for category in categories:
-	word_dict = countWord("C:\\Users\\Administrator\\Desktop\\python\\20_newsgroups\\"+category)
+	word_dict = countWord(docPath+category)
 	global all_word_dict
 	all_word_dict[category] = word_dict
 
@@ -76,12 +211,12 @@ def calRandomDocProbability():
 	total_count = 0
 	total_probability = 1.0
 	news_type = random.randint(0,19)# 产生一个随机数 0-19 代表 从20个新闻组中 随机选取一个
-	files = os.listdir("C:\\Users\\Administrator\\Desktop\\python\\20_newsgroups\\"+categories[news_type])# 列出选定的新闻组里面的1000个文档
+	files = os.listdir(docPath+categories[news_type])# 列出选定的新闻组里面的1000个文档
 	random_id = random.randint(500,999) #代表从后面的501到1000中 选取一个文档
 	for word in total_words:
 		doc_dict[word] = 0
 	for word in total_words:
-		for text in open("C:\\Users\\Administrator\\Desktop\\python\\20_newsgroups\\"+categories[news_type]+"\\"+files[random_id]).read().split():
+		for text in open(docPath+categories[news_type]+"\\"+files[random_id]).read().split():
 			if word.decode('utf-8') == text:
 				print('%s == %s' % (word,text))
 				total_count = total_count + 1
