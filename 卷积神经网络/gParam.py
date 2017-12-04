c342017afda5f475bd1f52cc0de8494fed520ec9
黄永宗(外包) <HUANGYONGZONG393@paicdom.local>
2017-12-04 17:51:39 +0800

    新增卷积神经网络和修改文档

---
+++ 卷积神经网络/gParam.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+TOP_PATH = '/media/autumn/Work/data/MNIST/mnist-png/'
+LAB_PATH = '/media/autumn/Work/data/MNIST/mnist-png/label1.txt'
+C_SIZE = 5
+F_NUM = 12
+P_SIZE = 2
+FILE_TYPE = '.png'
+MAX_ITER_NUM = 50
