1262258a453e0e382886c666b0d706b62371fdfb
黄永宗(外包) <HUANGYONGZONG393@paicdom.local>
2017-12-06 18:50:19 +0800

    新增CNN原理图和cnnPy注释

--- 卷积神经网络/myCnnTest.py
+++ 卷积神经网络/myCnnTest.py
@@ -8,7 +8,9 @@ import math
 import gParam
 
 # code
+#卷积层的数目
 cLyNum = 20
+#池化层的数目
 pLyNum = 20
 fLyNum = 100
 oLyNum = 10
@@ -16,27 +18,40 @@ train_num = 800
 
 myCnn = Ccnn(cLyNum, pLyNum, fLyNum, oLyNum)
 ylabel = myCnn.read_label(gParam.LAB_PATH)
+
+#循环处理800条数据
 for iter0 in range(gParam.MAX_ITER_NUM):
 	for i in range(train_num):
+		#读取图片数据
 		data = myCnn.read_pic_data(gParam.TOP_PATH, i)	
 		#print shape(data)
 		ylab = int(ylabel[i])
+		#取得 图片的行数和列数 28行*28列
 		d_m, d_n = shape(data)
+		#卷积之后的行数
 		m_c = d_m - gParam.C_SIZE + 1
+		#卷积之后的列数
 		n_c = d_n - gParam.C_SIZE + 1
+		#池化之后的行数
 		m_p = m_c/myCnn.pSize
+		#池化之后的列数
 		n_p = n_c/myCnn.pSize
 		state_c = zeros((m_c, n_c,myCnn.cLyNum))
 		state_p = zeros((m_p, n_p, myCnn.pLyNum))
 		for n in range(myCnn.cLyNum):
+			#对输入层进行卷积 20层 每层5行5列
 			state_c[:,:,n] = myCnn.convolution(data, myCnn.kernel_c[:,:,n])
 			#print shape(myCnn.cLyNum)
 			tmp_bias = ones((m_c,n_c)) * myCnn.cLyBias[:,n]
+			#对卷积之后的层 进行激活
 			state_c[:,:,n] = np.tanh(state_c[:,:,n] + tmp_bias)# 加上偏置项然后过激活函数
+			#对激活之后的卷积层 进行池化
 			state_p[:,:,n] = myCnn.pooling(state_c[:,:,n],myCnn.pooling_a)
-		state_f, state_f_pre = myCnn.convolution_f1(state_p,myCnn.kernel_f, myCnn.weight_f)
+		#两个返回分别是 1*1*100  12*12*100
+		state_f, state_f_pre = myCnn.convolution_f1(state_p, myCnn.kernel_f, myCnn.weight_f)
 		#print shape(state_f), shape(state_f_pre)
 		#进入激活函数
+		# 1 * 100
 		state_fo = zeros((1,myCnn.fLyNum))#全连接层经过激活函数的结果	
 		for n in range(myCnn.fLyNum):
 				state_fo[:,n] = np.tanh(state_f[:,:,n] + myCnn.fLyBias[:,n])
@@ -89,7 +104,7 @@ for i in test_num:
 	output = myCnn.softmax_layer(state_fo)	
 	#计算误差
 	y_pre = output.argmax(axis=1)
-	print '真实数字为%d',ylab, '预测数字是%d', y_pre
+	print('真实数字为%d',ylab, '预测数字是%d', y_pre)
 	
 
 
